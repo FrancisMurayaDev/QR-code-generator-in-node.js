4bcb3f43976e3af545c6af980496f2e055108f01
FrancisMurayaDev <francismurayadev@gmail.com>
2025-04-01 15:20:08 +0300

    use inquirer to get user input

---
+++ index.js
@@ -0,0 +1,16 @@
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    {"message": "Type in your URL", "name":"URL"}
+  ])
+  .then((answers) => {
+    const url = answers.URL
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
